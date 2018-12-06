e034b4ecf10d6c8a13add5ce3b490d020e2bfbc2
coryschw <acellama88@gmail.com>
2018-12-06 14:27:30 -0600

    Adding Day 6 Solutions

---
+++ AoC06/AoC06p2.py
@@ -0,0 +1,105 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+5.0% Complete
+3.3333333333333335% Complete
+2.5% Complete
+2.0% Complete
+1.6666666666666667% Complete
+1.4285714285714286% Complete
+1.25% Complete
+1.1111111111111112% Complete
+Distances Complete
+Location Area Calculated
+Traceback (most recent call last):
+  File "C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py", line 66, in <module>
+    Counts.pop(Grid[xLoc][yLoc])
+KeyError: 41
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+20.0% Complete
+30.0% Complete
+40.0% Complete
+50.0% Complete
+60.0% Complete
+70.0% Complete
+80.0% Complete
+90.0% Complete
+Distances Complete
+Location Area Calculated
+Traceback (most recent call last):
+  File "C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py", line 66, in <module>
+    Counts.pop(Grid[xLoc][yLoc])
+KeyError: 41
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+20.0% Complete
+30.0% Complete
+40.0% Complete
+50.0% Complete
+60.0% Complete
+70.0% Complete
+80.0% Complete
+90.0% Complete
+Distances Complete
+Location Area Calculated
+Traceback (most recent call last):
+  File "C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py", line 66, in <module>
+    Counts.pop(Grid[xLoc][yLoc])
+KeyError: 41
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+20.0% Complete
+30.0% Complete
+40.0% Complete
+50.0% Complete
+60.0% Complete
+70.0% Complete
+80.0% Complete
+90.0% Complete
+Distances Complete
+Location Area Calculated
+Removed infinite options
+Traceback (most recent call last):
+  File "C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py", line 88, in <module>
+    if Counts[xLoc] > most:
+KeyError: 4
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+20.0% Complete
+30.0% Complete
+40.0% Complete
+50.0% Complete
+60.0% Complete
+70.0% Complete
+80.0% Complete
+90.0% Complete
+Distances Complete
+Location Area Calculated
+Removed infinite options
+Traceback (most recent call last):
+  File "C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py", line 90, in <module>
+    most = Counts[xLoc]
+KeyError: 4
+>>> 
+========= RESTART: C:\Users\corysch\Documents\AoC18\AoC06\AoC06p1.py =========
+10.0% Complete
+20.0% Complete
+30.0% Complete
+40.0% Complete
+50.0% Complete
+60.0% Complete
+70.0% Complete
+80.0% Complete
+90.0% Complete
+Distances Complete
+Location Area Calculated
+Removed infinite options
+4233 21
+>>> 
