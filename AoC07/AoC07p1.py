8a972725be3f3685d85be1b72717430e66075a72
coryschw <acellama88@gmail.com>
2018-12-07 09:26:13 -0600

    Update AoC07p1.py

--- AoC07/AoC07p1.py
+++ AoC07/AoC07p1.py
@@ -2,14 +2,23 @@ class step:
     prereqs = list()
     def __init__(self, name):
         self.name = name
-    def isFree(self,req):
-        return len(prereqs)
+        self.prereqs = []
+    def isFree(self):
+        return len(self.prereqs)
     def addStep(self, req):
         self.prereqs.append(req)
     def remStep(self, req):
-        if req in self.prereqs:
-            self.prereqs.pop(req)
-        return len(prereqs)
+        i = 0
+        while i < len(self.prereqs):
+            if self.prereqs[i] == req:
+                del self.prereqs[i]
+            else:
+                i += 1
+        return len(self.prereqs)
+    def __lt__(self, other):
+        return self.name < other
+    def __gt__(self, other):
+        return self.name > other
 
 def getStep(name, queue):
     for x in queue:
@@ -25,21 +34,27 @@ queue = list()
 for x in alphabet:
     newClass = step(x)
     line.append(newClass)
+    #check
 for x in lines:
     tokens = x.split()
     getStep(tokens[7], line).addStep(tokens[1])
-for x in line:
-    print(x.prereqs[0])
-for x in line:
-    if x.isFree == 0:
-        line.pop(x)
-        queue.append(x)
+    #check
+i = 0
+while i < len(line):
+    if line[i].isFree() == 0:
+        queue.append(line[i])
+        del line[i]
+    else:
+        i += 1
 while len(queue) > 0:
     queue.sort()
     answer += queue[0].name
-    for x in line:
-        if x.remStep(queue[0].name) == 0:
-            line.pop(x)
-            queue.append(x)
+    i = 0
+    while i < len(line):
+        if line[i].remStep(queue[0].name) == 0:
+            queue.append(line[i])
+            del line[i]
+        else:
+            i += 1
     del queue[0]
 print("Answer is: " + answer)
